f178945a79e2972637a5cf203f28a9eca465b716
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-15 16:51:55 -0300

    feture(config):criando arquivos de configuração e adicionando .env

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-
+	config.Carregar()
+	fmt.Println(config.StringConexaoBanco)
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
 }
--- src/config/config.go
+++ src/config/config.go
@@ -1,10 +1,32 @@
 package config
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
+)
+
 var (
 	StringConexaoBanco = ""
 	Porta              = 0
 )
 
 func Carregar() {
+	var err error
 
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	Porta, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Porta = 8080
+	}
+	StringConexaoBanco = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_DATABASE"))
 }
