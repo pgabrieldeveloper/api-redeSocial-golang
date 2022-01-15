a932dfc5e1f61353208adf9a206835be17913175
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-15 17:36:56 -0300

    feature(repositorio):criando repositorio e adicionando metodo para cruar um usuario

--- src/controllers/usuariosController.go
+++ src/controllers/usuariosController.go
@@ -1,9 +1,37 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/db"
+	"api/src/models"
+	"api/src/repositorio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando um usuario"))
+
+	corpoRequisicao, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var usuario models.Usuario
+	if err = json.Unmarshal(corpoRequisicao, &usuario); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.Conectar()
+	if err != nil {
+		log.Fatal(err)
+	}
+	usuarioRepositorio := repositorio.NovoRepositorioDeUsuario(db)
+	usuarioID, err := usuarioRepositorio.Criar(usuario)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(fmt.Sprintf("ultimo id inserido: %d", usuarioID)))
 }
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando usuarios"))
