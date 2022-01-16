6a5990853a7b8ef8226e6172651237be3f829e05
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-01-16 08:38:29 -0300

    feature(Usuario):criando metodos para validar e formatar um usuario

--- src/models/Usuario.go
+++ src/models/Usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //Usuario modele de um usuario na api
 type Usuario struct {
@@ -10,4 +14,36 @@ type Usuario struct {
 	Email    string    `json:email,omitempty`
 	Password string    `json:password,omitempty`
 	CreateAt time.Time `json:creatAt,omitempty`
+}
+
+//Preparar valida e prepara campos para ser inseridos no banco de dados
+func (u *Usuario) Preparar() error {
+
+	if err := u.validar(); err != nil {
+		return err
+	}
+	u.formatar()
+	return nil
+}
+
+func (u *Usuario) validar() error {
+	if u.Name == "" {
+		return errors.New("O campo name é um campo obrigatorio e nao pode estar em branco")
+	}
+	if u.Nick == "" {
+		return errors.New("O campo nick é um campo obrigatorio e nao pode estar em branco")
+	}
+	if u.Email == "" {
+		return errors.New("O campo email é um campo obrigatorio e nao pode estar em branco")
+	}
+	if u.Password == "" {
+		return errors.New("O campo password é um campo obrigatorio e nao pode estar em branco")
+	}
+	return nil
+}
+
+func (u *Usuario) formatar() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Nick = strings.TrimSpace(u.Nick)
 }
